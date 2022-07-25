a305846664ecdab1c66a06bd9c43bb85790068bc
Zach Cossman <Zachcos@gmail.com>
2022-07-25 19:04:45 -0300

    Add head tag with site metadata and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <html lang='en' />
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
+        <meta
+          name='description'
+          content='Zach Cossman Designs - Custom performant websites'
+        />
+        <title>Zach Cossman Designs</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
