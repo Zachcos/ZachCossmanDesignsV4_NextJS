5f59d7b47cd79e9ec2a1ddc5454c9e151a9e399d
Zach Cossman <Zachcos@gmail.com>
2022-07-26 14:16:43 -0300

    Add CtaBtn component and style files

---
+++ components/ctaBtn.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/CtaBtn.module.css';
+
+const CtaBtn = () => {
+  return <button className={styles.ctaBtn}>zachcos@gmail.com</button>;
+};
+
+export default CtaBtn;
