06198c5364f1f61dd264e7d124b29738e1438326
Zach Cossman <Zachcos@gmail.com>
2022-07-26 14:01:11 -0300

    Add initial page structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>This is the home page</h1>
+    <div className={styles.heroContainer}>
+      <h1 className={styles.heroDisplayText}>
+        Hi, I'm Zach — I design and develop websites
+      </h1>
+      <p className={styles.heroDisplayCopy}>
+        For over 10 years I've been designing and developing sites and web
+        experiences for companies and creative individuals. Check out my work,
+        learn more{' '}
+        <Link href='/about' aria-label='about'>
+          <a>about me</a>
+        </Link>
+        , and get in touch...
+      </p>
+      {/* <CtaBtn /> */}
     </div>
   );
 };
