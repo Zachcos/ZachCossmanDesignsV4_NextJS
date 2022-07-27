3e0a28025b1d737e079bc989707fd8ebfaf0d492
Zach Cossman <Zachcos@gmail.com>
2022-07-27 00:42:20 -0300

    Create Work page and styles files

---
+++ pages/work.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Work.module.css';
+import type { NextPage } from 'next';
+
+const Work: NextPage = () => {
+  return (
+    <>
+      <h1>this is the work page</h1>
+    </>
+  );
+};
+
+export default Work;
