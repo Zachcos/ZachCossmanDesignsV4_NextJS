dacf4e939401ba04ef330976acedaddca09033b3
Zach Cossman <Zachcos@gmail.com>
2022-07-27 12:10:43 -0300

    Wire up button to open email editor

--- components/ctaBtn.tsx
+++ components/ctaBtn.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import styles from '../styles/CtaBtn.module.css';
 
 const CtaBtn = () => {
-  //TODO: Figure out how to send email from <button> for semantic clairty
   function handleEmail() {
-    console.log('the button was clicked!');
+    window.location.href = 'mailto: Zachcos@gmail.com?subject=Hey Zach!';
   }
 
   return (
