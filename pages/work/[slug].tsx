0e9b77c5110e1c419e3462f15e51fcfe77385a1d
Zach Cossman <Zachcos@gmail.com>
2022-07-28 03:17:50 -0300

    Add page structure and styles for [slug]-generated pages

--- pages/work/[slug].tsx
+++ pages/work/[slug].tsx
@@ -1,18 +1,92 @@
 import React from 'react';
 import type { GetStaticProps, GetStaticPaths } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
 import { featuredProjectData } from '../../data/data';
+import styles from '../../styles/ProjectDetail.module.css';
+import { Arrow } from '../../components/icons';
+import Footer from '../../components/footer';
 
 interface Props {
   project: FeaturedItemProps;
 }
 
 const ProjectDetail = ({ project }: Props) => {
+  const { title, description, liveUrl, nextProj, assets } = project;
   return (
     <>
-      <h1 style={{ marginTop: '400px' }}>
-        This is a dog named {project.title}
-      </h1>
-      <p>He is: {project.description}</p>
+      <div
+        className={styles.detailsContainer}
+        // variants={aniVariants.parent}
+        // initial='initial'
+        // animate='animate'
+        // exit='exit'
+      >
+        <div
+          // variants={aniVariants.parent}
+          className={styles.info}
+        >
+          <h2
+            // variants={aniVariants.child}
+            className={styles.title}
+          >
+            {title}
+          </h2>
+          <div
+            // variants={aniVariants.child}
+            className={styles.field}
+          >
+            <span className={styles.fieldLabel}>liveUrl</span>
+            <span className={styles.fieldInfo}>
+              <a href={liveUrl} target='_blank' rel='noreferrer'>
+                {liveUrl}
+              </a>
+            </span>
+          </div>
+        </div>
+        <div
+          // variants={aniVariants.parent}
+          className={styles.description}
+        >
+          {description.map((para: string, index: number) => (
+            <p
+              key={index}
+              // variants={aniVariants.child}
+            >
+              {para}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div
+        className={styles.assetContainer}
+        // variants={aniVariants.child}
+      >
+        {assets.map((asset: string, index: number) => (
+          <Image
+            src={asset}
+            alt=''
+            key={index}
+            width={3200}
+            height={1550}
+            objectFit='contain'
+          />
+        ))}
+      </div>
+      <div className={styles.nextContainer}>
+        <div className={styles.nextDetails}>
+          <div className={styles.nextLabel}>Next Project</div>
+          <div className={styles.nextProjectName}>
+            <Link href={`/work${nextProj.slug}`}>
+              <a>
+                {nextProj.title}
+                <Arrow />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
