57f07fcb705bbf544e9cbb285653783602c72c24
Zach Cossman <Zachcos@gmail.com>
2022-07-28 00:43:22 -0300

    Remove tag in <Head> that was causing error to be thrown

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <html lang='en' />
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
