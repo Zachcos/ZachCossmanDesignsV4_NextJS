400fb1873b39a3a552e0b96204c9db383d992781
Zach Cossman <Zachcos@gmail.com>
2022-07-29 18:26:23 -0300

    Adjust animation timing and add y: -20 for exit animations

--- data/animations.ts
+++ data/animations.ts
@@ -2,8 +2,9 @@ const easing = [0.6, -0.5, 0.01, 0.99];
 
 const fadeUpIn = {
   initial: { y: 20, opacity: 0 },
-  animate: { y: 0, opacity: 1, transition: { duration: 0.6, ease: easing } },
+  animate: { y: 0, opacity: 1, transition: { duration: 0.45, ease: easing } },
   exit: {
+    y: -20,
     opacity: 0,
     transition: { duration: 0.4 },
   },
@@ -14,11 +15,11 @@ const fadeUpInWithChildren = {
   animate: {
     y: 0,
     opacity: 1,
-    transition: { duration: 0.6, ease: easing, staggerChildren: 0.05 },
+    transition: { duration: 0.45, ease: easing, staggerChildren: 0.05 },
   },
   exit: {
     opacity: 0,
-    transition: { duration: 0.4 },
+    transition: { duration: 0.45 },
   },
 };
 
