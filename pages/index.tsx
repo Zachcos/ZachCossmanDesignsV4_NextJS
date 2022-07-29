603861559a49293d174860f2908fb28b5dbd8243
Zach Cossman <Zachcos@gmail.com>
2022-07-28 22:57:27 -0300

    Add motion components to allow page animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,37 @@
+import React from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import { motion } from 'framer-motion';
 import CtaBtn from '../components/ctaBtn';
+import { easing, fadeUpIn, stagger } from '../data/animations';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.heroContainer}>
-      <h1 className={styles.heroDisplayText}>
-        Hi, I'm Zach — I design and develop websites
-      </h1>
-      <p className={styles.heroDisplayCopy}>
-        For over 10 years I've been designing and developing sites and web
-        experiences for companies and creative individuals. Check out my work,
-        learn more{' '}
-        <Link href='/about' aria-label='about'>
-          <a className={styles.aboutLink}>about me</a>
-        </Link>
-        , and get in touch...
-      </p>
-      <CtaBtn />
-    </div>
+    <motion.div
+      className={styles.heroContainer}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+    >
+      <motion.div variants={stagger}>
+        <motion.h1 className={styles.heroDisplayText} variants={fadeUpIn}>
+          Hi, I'm Zach — I design and develop websites
+        </motion.h1>
+        <motion.p className={styles.heroDisplayCopy} variants={fadeUpIn}>
+          For over 10 years I've been designing and developing sites and web
+          experiences for companies and creative individuals. Check out my work,
+          learn more{' '}
+          <Link href='/about' aria-label='about' passHref>
+            <a className={styles.aboutLink}>about me</a>
+          </Link>
+          , and get in touch...
+        </motion.p>
+        <motion.div variants={fadeUpIn}>
+          <CtaBtn />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
