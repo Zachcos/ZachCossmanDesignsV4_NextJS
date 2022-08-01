08eba53968ca6990b16dd96208d9a8ae3f925cd5
Zach Cossman <Zachcos@gmail.com>
2022-08-01 14:15:58 -0400

    Refactor Work/index to styled-components

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../styles/Work.module.css';
+import styled from 'styled-components';
 import { GetStaticProps } from 'next';
 import type { NextPage } from 'next';
 import Image from 'next/image';
@@ -9,6 +9,74 @@ import { featuredProjectData, otherProjectData } from '../../data/data';
 import { fadeUpIn, fadeUpInWithChildren, stagger } from '../../data/animations';
 import FeaturedItem from '../../components/featuredItem';
 import Footer from '../../components/footer';
+
+const PageWrapper = styled(motion.div)`
+  .workWrapper {
+    margin: 270px 0 200px 0;
+    position: relative;
+    width: 100%;
+  }
+  .featuredWrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin-bottom: 150px;
+    position: relative;
+    width: 100%;
+  }
+  .wrapperHeading {
+    color: var(--color-accent);
+    font-family: 'Oswald', sans-serif;
+    font-size: var(--font-h2);
+    margin-bottom: 100px;
+    position: relative;
+    text-transform: uppercase;
+    width: 100%;
+  }
+  .otherWrapper {
+    align-items: center;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 100%;
+  }
+  .otherProjectWrapper {
+    height: 275px;
+    margin: 25px;
+    overflow: hidden;
+    width: 275px;
+    img {
+      transition: 0.15s ease-out;
+    }
+    &:hover img {
+      transform: scale(1.1);
+    }
+  }
+  @media screen and (max-width: 856px) {
+    .otherProjectWrapper {
+      height: 200px;
+      margin: 15px;
+      width: 200px;
+    }
+  }
+  @media screen and (max-width: 630px) {
+    .workWrapper {
+      margin: 170px 0 100px 0;
+    }
+    .featuredWrapper {
+      margin-bottom: 100px;
+    }
+    .wrapperHeading {
+      font-size: var(--font-h3);
+      text-align: center;
+    }
+    .otherProjectWrapper {
+      height: 130px;
+      margin: 15px 0;
+      width: 130px;
+    }
+  }
+`;
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
@@ -26,19 +94,16 @@ interface Props {
 
 const Work: NextPage<Props> = ({ featured, other }) => {
   return (
-    <motion.div
+    <PageWrapper
       initial='initial'
       animate='animate'
       exit='exit'
       variants={stagger}
     >
-      <motion.div className={styles.workWrapper} variants={fadeUpIn}>
-        <motion.div
-          className={styles.featureWrapper}
-          variants={fadeUpInWithChildren}
-        >
+      <motion.div className='workWrapper' variants={fadeUpIn}>
+        <motion.div className='featureWrapper' variants={fadeUpInWithChildren}>
           <motion.div
-            className={styles.wrapperHeading}
+            className='wrapperHeading'
             variants={fadeUpInWithChildren}
           >
             Featured Projects
@@ -47,10 +112,10 @@ const Work: NextPage<Props> = ({ featured, other }) => {
             <FeaturedItem key={item.id} {...item} />
           ))}
         </motion.div>
-        <div className={styles.otherWrapper}>
-          <div className={styles.wrapperHeading}>Other Projects</div>
+        <div className='otherWrapper'>
+          <div className='wrapperHeading'>Other Projects</div>
           {other.map((item: OtherItemProps) => (
-            <div className={styles.otherProjectWrapper} key={item.id}>
+            <div className='otherProjectWrapper' key={item.id}>
               <a href={item.behanceUrl} target='_blank' rel='noreferrer'>
                 <Image
                   src={item.thumbImg}
@@ -65,7 +130,7 @@ const Work: NextPage<Props> = ({ featured, other }) => {
         </div>
       </motion.div>
       <Footer />
-    </motion.div>
+    </PageWrapper>
   );
 };
 
