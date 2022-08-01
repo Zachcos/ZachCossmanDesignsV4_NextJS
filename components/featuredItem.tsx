479e26044a2a06c92c31fa3b346c23094277690f
Zach Cossman <Zachcos@gmail.com>
2022-08-01 12:37:10 -0400

    Refactor featuredItem component to styled-components

--- components/featuredItem.tsx
+++ components/featuredItem.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/FeaturedItem.module.css';
+import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { fadeUpIn } from '../data/animations';
+
+const FeaturedItemWrapper = styled(motion.div)`
+  height: 550px;
+  margin: 0 0 100px 0;
+  position: relative;
+  width: 100%;
+  .imageFrame {
+    height: 550px;
+    margin-bottom: 50px;
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    img {
+      height: 100%;
+      object-fit: cover;
+      position: relative;
+      top: 0;
+      transition: 0.3s ease-out;
+      width: 100%;
+      &:hover {
+        transform: scale(1.05);
+      }
+    }
+  }
+  .title {
+    color: var(--color-darkGrey);
+    font-family: 'Oswald', sans-serif;
+    font-size: var(--font-base);
+    font-weight: 100;
+    margin-bottom: 20px;
+    position: absolute;
+    right: 30px;
+    top: 30px;
+  }
+  @media screen and (max-width: 630px) {
+    height: 350px;
+
+    .imageFrame {
+      height: 350px;
+    }
+  }
+`;
 
 interface Props {
   slug: string;
@@ -13,16 +55,16 @@ interface Props {
 
 const FeaturedItem = ({ slug, portImg, title }: Props) => {
   return (
-    <motion.div className={styles.wrapper} variants={fadeUpIn}>
+    <FeaturedItemWrapper variants={fadeUpIn}>
       <Link href={`/work/${slug}`} scroll={false}>
         <a>
-          <div className={styles.imageFrame}>
+          <div className='imageFrame'>
             <Image src={portImg} alt='' layout='fill' objectFit='cover' />
           </div>
-          <div className={styles.title}>{title}</div>
+          <div className='title'>{title}</div>
         </a>
       </Link>
-    </motion.div>
+    </FeaturedItemWrapper>
   );
 };
 
