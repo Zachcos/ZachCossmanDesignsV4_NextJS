a2fc18ed100a329daaf3b9f885872586f3626ab0
Zach Cossman <Zachcos@gmail.com>
2022-08-01 12:54:33 -0400

    Refactor Icons component to styled-components

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,41 +1,60 @@
 import React from 'react';
-import styles from '../styles/Icons.module.css';
+import styled from 'styled-components';
+
+const SocialIcon = styled.svg`
+  fill: var(--color-lightGrey);
+  height: 24px;
+  transition: fill 0.15s ease-out;
+  width: 24px;
+`;
+
+const ArrowIcon = styled.svg`
+  fill: var(--color-lightGrey);
+  height: 28px;
+  width: 28px;
+`;
+
+const LogoIcon = styled.svg`
+  fill-rule: evenodd;
+  clip-rule: evenodd;
+  stroke-linejoin: round;
+  stroke-miterlimit: 2;
+  fill: var(--color-accent);
+  width: 32px;
+  #Layer-2 path {
+    fill: var(--color-accent);
+  }
+`;
 
 const Behance = () => {
   return (
-    <svg
+    <SocialIcon
       xmlns='http://www.w3.org/2000/svg'
       width='24'
       height='24'
       viewBox='0 0 24 24'
-      className={styles.social}
     >
       <path d='M22 7h-7v-2h7v2zm1.726 10c-.442 1.297-2.029 3-5.101 3-3.074 0-5.564-1.729-5.564-5.675 0-3.91 2.325-5.92 5.466-5.92 3.082 0 4.964 1.782 5.375 4.426.078.506.109 1.188.095 2.14h-8.027c.13 3.211 3.483 3.312 4.588 2.029h3.168zm-7.686-4h4.965c-.105-1.547-1.136-2.219-2.477-2.219-1.466 0-2.277.768-2.488 2.219zm-9.574 6.988h-6.466v-14.967h6.953c5.476.081 5.58 5.444 2.72 6.906 3.461 1.26 3.577 8.061-3.207 8.061zm-3.466-8.988h3.584c2.508 0 2.906-3-.312-3h-3.272v3zm3.391 3h-3.391v3.016h3.341c3.055 0 2.868-3.016.05-3.016z' />
-    </svg>
+    </SocialIcon>
   );
 };
 
 const Github = () => {
   return (
-    <svg
+    <SocialIcon
       xmlns='http://www.w3.org/2000/svg'
       width='24'
       height='24'
       viewBox='0 0 24 24'
-      className={styles.social}
     >
       <path d='M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z' />
-    </svg>
+    </SocialIcon>
   );
 };
 
 const Arrow = () => {
   return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      viewBox='0 0 31.143 31.143'
-      className={styles.arrow}
-    >
+    <ArrowIcon xmlns='http://www.w3.org/2000/svg' viewBox='0 0 31.143 31.143'>
       <g>
         <g>
           <path
@@ -46,13 +65,13 @@ const Arrow = () => {
           />
         </g>
       </g>
-    </svg>
+    </ArrowIcon>
   );
 };
 
 const Logo = () => {
   return (
-    <svg
+    <LogoIcon
       width='100%'
       height='100%'
       viewBox='0 0 707 1000'
@@ -60,14 +79,13 @@ const Logo = () => {
       xmlns='http://www.w3.org/2000/svg'
       xmlnsXlink='http://www.w3.org/1999/xlink'
       xmlSpace='preserve'
-      className={styles.logo}
     >
       <g transform='matrix(2.00448,0,0,2.00445,-1.81867,-0.843833)'>
         <g id='Layer-2'>
           <path d='M0.907,0.84L0.907,25.07L353.119,25.07L353.119,0.84L0.907,0.84ZM353.119,103.931L141.415,396.12L264.904,396.12C270.516,396.12 274.284,393.665 274.908,389.3L286.73,322.521L353.119,322.521L353.119,103.931ZM104.648,106.14C99.036,106.14 95.268,108.66 94.644,113.026L82.822,171.035L0.907,173.503L0.907,398.914L213.131,106.14L104.648,106.14ZM0.907,476.021L0.907,499.731L353.119,499.731L353.119,476.605L0.907,476.021Z' />
         </g>
       </g>
-    </svg>
+    </LogoIcon>
   );
 };
 
